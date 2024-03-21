cb96a2546360acf0f18532abc533fc642f2f1557
”Frederik <fred295q@stud.kea.dk>
2024-03-21 10:25:17 +0100

    Mouse over text with background and size change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Cursor from "./components/Cursor";
 
 function App() {
+  const [isOver, setIsOver] = useState(false);
+
   return (
     <section className="grid w-screen h-screen bg-bgcol">
-      <h1 className="text-7xl place-self-center text-textcol">Discover the world of Novelty Marketing</h1>
-      <Cursor />
+      <h1
+        onMouseEnter={() => {
+          setIsOver(true);
+        }}
+        onMouseLeave={() => {
+          setIsOver(false);
+        }}
+        className="text-center text-7xl place-self-center text-textcol"
+      >
+        Discover the world of Novelty Marketing
+      </h1>
+      <Cursor isOver={isOver} />
     </section>
   );
 }
--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -3,10 +3,16 @@ import { useRef } from "react";
 
 import { useEffect } from "react";
 
-function Cursor() {
-  const size = 30;
+function Cursor({ isOver }) {
+  console.log(isOver);
+  const size = isOver ? 300 : 30;
   const cursorFollow = useRef();
   const mouse = useRef({
+    x: 0,
+    y: 0,
+  });
+
+  const delay = useRef({
     x: 0,
     y: 0,
   });
@@ -17,28 +23,42 @@ function Cursor() {
       x: clientX,
       y: clientY,
     };
-    moveMouse(mouse.current.x, mouse.current.y);
-    console.log(mouse.current.x, mouse.current.y);
   };
+
+  const lerp = (x, y, a) => x * (1 - a) + y * a;
 
   const moveMouse = (x, y) => {
     gsap.set(cursorFollow.current, { x, y, xPercent: -50, yPercent: -50 });
   };
 
-  const animate = () => {};
+  const animate = () => {
+    const { x, y } = delay.current;
+
+    delay.current = {
+      x: lerp(x, mouse.current.x, 0.075),
+      y: lerp(y, mouse.current.y, 0.075),
+    };
+
+    moveMouse(delay.current.x, delay.current.y);
+
+    window.requestAnimationFrame(animate);
+  };
 
   useEffect(() => {
+    animate();
     window.addEventListener("mousemove", manageMouseMove);
     return () => window.removeEventListener("mousemove", manageMouseMove);
-  }, []);
+  }, [isOver]);
 
   return (
     <div
       ref={cursorFollow}
-      className="fixed top-0 left-0 z-40 border-2 rounded-full border-textcol"
+      className="fixed top-0 left-0 z-40 border-2 rounded-full pointer-events-none border-textcol mix-blend-difference"
       style={{
         width: size,
         height: size,
+        transition: "width 0.5s ease, height 0.5s ease",
+        backgroundColor: isOver ? "#FBF4DA" : "transparent",
       }}
     ></div>
   );
